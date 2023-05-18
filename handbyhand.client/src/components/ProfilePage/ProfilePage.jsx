c7425540c2b648ee3cf048593032615c2bc04cb6
yuvalohana <42322357+yuvalohana@users.noreply.github.com>
2023-05-18 15:56:04 +0300

    start profile page

--- handbyhand.client/src/components/ProfilePage/ProfilePage.jsx
+++ handbyhand.client/src/components/ProfilePage/ProfilePage.jsx
@@ -1,80 +1,51 @@
 import React from 'react'
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
 import './ProfilePage.css';
 
-import ProfileNavBar from './ProfileNavBar';
-
- const ProfilePage = () => {
-  const [activeTab, setActiveTab] = useState('profile');
-
-  const handleTabClick = (tabName) => {
-    setActiveTab(tabName);
-  };
+const ProfilePage = () => {
 
   return (
-    <div className="container-fluid">
-      <div className="row">
-        <nav className="col-md-2 d-none d-md-block bg-light sidebar">
-          <div className="sidebar-sticky">
-            <ul className="nav flex-column">
-              <li className="nav-item">
-                <Link
-                  className={`nav-link ${activeTab === 'profile' ? 'active' : ''}`}
-                  to="#"
-                  onClick={() => handleTabClick('profile')}
-                >
-                  Profile
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  className={`nav-link ${activeTab === 'settings' ? 'active' : ''}`}
-                  to="#"
-                  onClick={() => handleTabClick('settings')}
-                >
-                  Settings
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  className={`nav-link ${activeTab === 'notifications' ? 'active' : ''}`}
-                  to="#"
-                  onClick={() => handleTabClick('notifications')}
-                >
-                  Notifications
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </nav>
+    <div className="profilepage-container">
 
-        <main role="main" className="col-md-9 ml-sm-auto col-lg-10 px-4">
-          <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-            <h1 className="h2">{activeTab === 'profile' ? 'Profile' : activeTab === 'settings' ? 'Settings' : 'Notifications'}</h1>
+      <div className="profilepages-form">
+        <h1 id="profile-page-title">My Profile</h1>
+
+        <form>
+          <div className="about form-group">
+            <h5 id="profile-page-title">About</h5>
+            <label className="textfield-lab" htmlFor="firstname">First name: </label>
+            {/* <label className="login-lab" htmlFor="author">user.firstמame</label> */}
+            <label className="textfield-lab" htmlFor="firstname">Last name: </label>
+            {/* <label className="login-lab" htmlFor="author">user.lastמame</label> */}
+            <label className="textfield-lab" htmlFor="author">Birth Date: </label>
+            {/* <label className="login-lab" htmlFor="author">user.birthdate</label> */}
+            <label className="textfield-lab" htmlFor="author">Gender: </label>
+            {/* <label className="login-lab" htmlFor="author">user.gender</label> */}
           </div>
-          {activeTab === 'profile' && (
-            <div>
-              <h2>Profile information</h2>
-              {/* display user profile information */}
-            </div>
-          )}
-          {activeTab === 'settings' && (
-            <div>
-              <h2>Settings</h2>
-              {/* display user settings */}
-            </div>
-          )}
-          {activeTab === 'notifications' && (
-            <div>
-              <h2>Notifications</h2>
-              {/* display user notifications */}
-            </div>
-          )}
-        </main>
+
+          <div className="contact-info form-group">
+            <h5 id="profile-page-title">Contact info</h5>
+            <label className="textfield-lab" htmlFor="author">Email: </label>
+            {/* <label className="login-lab" htmlFor="author">user.email</label> */}
+            <label className="textfield-lab" htmlFor="author">Mobile: </label>
+            {/* <label className="login-lab" htmlFor="author">user.mobile</label> */}
+          </div>
+
+          <div className="skills form-group">
+          <h5 id="profile-page-title">Skills</h5>
+          </div>
+
+          <div className="future-events form-group">
+          <h5 id="profile-page-title">Events I'm going to attend</h5>
+          </div>
+
+          <div className="past-events form-group">
+          <h5 id="profile-page-title">Events I attended</h5>
+          </div>
+
+        </form>
       </div>
     </div>
-  );
+  )
 }
 
 export default ProfilePage;
