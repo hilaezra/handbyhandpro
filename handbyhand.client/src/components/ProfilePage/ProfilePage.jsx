d3ec3590e4dd13d8781cec928b89d07f3b9810bf
yuvalohana <42322357+yuvalohana@users.noreply.github.com>
2023-05-19 16:21:28 +0300

    css profile page and creat request to the server

--- handbyhand.client/src/components/ProfilePage/ProfilePage.jsx
+++ handbyhand.client/src/components/ProfilePage/ProfilePage.jsx
@@ -1,33 +1,38 @@
 import React from 'react'
+import axios from 'axios'
 import './ProfilePage.css';
 
-const ProfilePage = () => {
+const ProfilePage = async () => {
+
+  //Get the conected user from server
+  //TODO- CREATE THE REQUEST IN THE SERVER
+  // const user = await axios.post("http://localhost:3000/auth/signup", { }) 
 
   return (
     <div className="profilepage-container">
 
-      <div className="profilepages-form">
+      <div className="profilepage-form">
         <h1 id="profile-page-title">My Profile</h1>
 
         <form>
           <div className="about form-group">
             <h5 id="profile-page-title">About</h5>
             <label className="textfield-lab" htmlFor="firstname">First name: </label>
-            {/* <label className="login-lab" htmlFor="author">user.firstמame</label> */}
+            {/* <label className="user-details" htmlFor="author">user.firstname</label> */}
             <label className="textfield-lab" htmlFor="firstname">Last name: </label>
-            {/* <label className="login-lab" htmlFor="author">user.lastמame</label> */}
+            {/* <label className="user-details" htmlFor="author">user.lastname</label> */}
             <label className="textfield-lab" htmlFor="author">Birth Date: </label>
-            {/* <label className="login-lab" htmlFor="author">user.birthdate</label> */}
+            {/* <label className="user-details" htmlFor="author">user.birthdate</label> */}
             <label className="textfield-lab" htmlFor="author">Gender: </label>
-            {/* <label className="login-lab" htmlFor="author">user.gender</label> */}
+            {/* <label className="user-details" htmlFor="author">user.gender</label> */}
           </div>
 
           <div className="contact-info form-group">
             <h5 id="profile-page-title">Contact info</h5>
             <label className="textfield-lab" htmlFor="author">Email: </label>
-            {/* <label className="login-lab" htmlFor="author">user.email</label> */}
+            {/* <label className="user-details" htmlFor="author">user.email</label> */}
             <label className="textfield-lab" htmlFor="author">Mobile: </label>
-            {/* <label className="login-lab" htmlFor="author">user.mobile</label> */}
+            {/* <label className="user-details" htmlFor="author">user.mobile</label> */}
           </div>
 
           <div className="skills form-group">
